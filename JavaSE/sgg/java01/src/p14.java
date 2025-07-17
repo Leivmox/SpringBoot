ae7af3907f0aec7adfab82d2e086b1fb38ae078a
Leivmox <leivmox@gmail.com>
2025-07-17 12:38:11 +0800

    update

---
+++ JavaSE/sgg/java01/src/p14.java
@@ -0,0 +1,15 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class p14 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+}
+
+class a {
+    public static void main(String[] args) {
+        System.out.println("Hello World111");
+    }
+}
